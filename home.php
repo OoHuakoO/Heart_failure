90565d5cf02bddc3645295a86297c52760d0ad5e
khomsark <komsark_@hotmail.co.th>
2020-12-17 01:44:52 +0700

    commit

--- home.php
+++ home.php
@@ -17,24 +17,22 @@
     #create h-input.arff file to save data for prediction
     $inputfile = fopen("h-input.arff","w") or die("Unable to open file!");
     $text = '
-        @RELATION HeartDisease
-        @ATTRIBUTE age NUMERIC
-        @ATTRIBUTE sex {0,1}
-        @ATTRIBUTE cp {0,1,2,3}
-        @ATTRIBUTE trestbps NUMERIC
-        @ATTRIBUTE chol NUMERIC
-        @ATTRIBUTE fbs {0,1}
-        @ATTRIBUTE restecg {0,1,2}
-        @ATTRIBUTE thalach NUMERIC
-        @ATTRIBUTE exang {0,1}
-        @ATTRIBUTE oldpeak NUMERIC
-        @ATTRIBUTE slope {0,1,2}
-        @ATTRIBUTE ca NUMERIC
-        @ATTRIBUTE thal {0,1,2,3}
-        @ATTRIBUTE target {0,1}
+        @RELATION heart_failure
+        @attribute sex {0,1}
+        @attribute age numeric
+        @attribute anaemia {0,1}
+        @attribute creatinine_phosphokinase numeric
+        @attribute diabetes {0,1}
+        @attribute ejection_fraction numeric
+        @attribute high_blood_pressure {0,1}
+        @attribute platelets numeric
+        @attribute serum_creatinine numeric
+        @attribute serum_sodium numeric
+        @attribute smoking {0,1}
+        @attribute DEATH_EVENT {0,1}
         @DATA '
-        .$age.','.$sex.','.$cp.','.$trestbps.','.$chol.','.$fbs.','.$restecg
-        .','.$thalach.','.$exang.','.$oldpeak.','.$slope.','.$ca.','.$thal.',?';
+           .$sex.','.$age.','.$redblood.','.$ensyne.','.$sweet.','.$heart.','.$kwamdan.','.$ketblood
+        .','.$creatine.','.$sodium.','.$smokeweed.',?';
     fwrite($inputfile,$text);
     fclose($inputfile);
 
